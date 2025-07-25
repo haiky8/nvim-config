e4c1ee899e649d9d97db8e25d83fa148bc5fa881
haiky8 <samuelcorreo2010@gmail.com>
2025-07-25 12:10:02 -0400

    Añadida configuración inicial

---
+++ lua/plugins/coderunner.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "CRAG666/code_runner.nvim",
+    lazy = true,
+    config = function()
+      require("code_runner").setup {
+        filetype = {
+          python = "python3 $FILENAME",
+          c = "clang \"$file\" -o \"$dir/$fileNameWithoutExt\" && \"$dir/$fileNameWithoutExt\"",
+          cpp = "clang++ \"$file\" -o \"$dir/$fileNameWithoutExt\" && \"$dir/$fileNameWithoutExt\"",
+       },
+      }
+    end,
+    keys = {
+      { "<leader>rr", "<cmd>RunCode<cr>", desc = "Run Code" },
+    },
+  }
+}
+
